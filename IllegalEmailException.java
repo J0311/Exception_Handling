2c5f52f13beba4fa681fe379260e06e4b6149fbc
Joseph Walker <96276860+J0311@users.noreply.github.com>
2022-06-15 13:21:40 -0400

    Add files via upload

---
+++ IllegalEmailException.java
@@ -0,0 +1,11 @@
+// Creating our own user - defined Exception 
+
+
+public class IllegalEmailException extends Exception {
+	
+	public IllegalEmailException(String message) {
+		
+		super(message);
+	}
+
+}
